55d1fef49c794828f8afc178fb597aed8de9000d
bunchlc <bun15009@byui.edu>
2019-06-19 13:35:49 -0600

    asg

---
+++ homework/js-practice/practice.js
@@ -0,0 +1,9 @@
+let string1 = "Wake me up";
+let string2 = "Wake me up inside";
+
+stringCombine(string1, string2);
+
+function stringCombine(string1, string2) {
+   let fullString = string1 + " " + string2;
+   console.log(fullString);
+}
