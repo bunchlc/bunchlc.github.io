2a1b5507cd7baad073c451a3ea2fc390ad8d9691
bunchlc <bun15009@byui.edu>
2019-06-28 13:44:46 -0600

    CLeaning things up

--- weather/js/functions.js
+++ weather/js/functions.js
@@ -6,12 +6,13 @@ console.log('My javascript is being read.');
 // variables for function use
 let date = new Date();
 let nextHour = date.getHours() + 1;
+var storage = window.localStorage;
 
-// let speed = 51;
-// let temp = 40;
-// let direction = "SW";
-// let condition = "Rain";
-
+var idHeader = {
+   headers: {
+      "User-Agent": "Student Learning Project - bun15009@byui.edu"
+   }
+};
 
 // Calculates wind chill
 function buildWC(speed, temp) {
@@ -209,3 +210,92 @@ function buildHourlyData(nextHour, hourlyTemps) {
    console.log('hourlyList is: ' + hourlyListItems);
    return hourlyListItems;
 }
+
+// Gets location information from the NWS API
+function getLocation(locale) {
+   const URL = "https://api.weather.gov/points/" + locale;
+   // NWS User-Agent header (built above) will be the second parameter 
+   fetch(URL, idHeader)
+      .then(function (response) {
+         if (response.ok) {
+            return response.json();
+         }
+         throw new ERROR('Response not OK.');
+      })
+      .then(function (data) {
+         // Let's see what we got back
+         console.log('Json object from getLocation function:');
+         console.log(data);
+         // Store data to localstorage 
+         storage.setItem("locName", data.properties.relativeLocation.properties.city);
+         storage.setItem("locState", data.properties.relativeLocation.properties.state);
+
+         // Next, get the weather station ID before requesting current conditions 
+         // URL for station list is in the data object 
+         let stationsURL = data.properties.observationStations;
+         // Call the function to get the list of weather stations
+         getStationId(stationsURL);
+      })
+      .catch(error => console.log('There was a getLocation error: ', error))
+} // end getLocation function
+
+// Gets weather station list and the nearest weather station ID from the NWS API
+function getStationId(stationsURL) {
+   // NWS User-Agent header (built above will be the second parameter
+   fetch(stationsURL, idHeader)
+      .then(function (response) {
+         if (response.ok) {
+            return response.json();
+         }
+         throw new ERROR('Response not OK.');
+      })
+      .then(function (data) {
+         // Let's see what we got back
+         console.log('From getStationId function:');
+         console.log(data);
+
+         // Store station ID and elevation(in meters - will need to be converted to feet)
+         let stationId = data.features[0].properties.stationIdentifier;
+         let stationElevation = data.features[0].properties.elevation.value;
+         console.log('Station and Elevation are: ' + stationId, stationElevation);
+
+         // Store data to localstorage
+         storage.setItem("stationId", stationId);
+         storage.setItem("stationElevation", stationElevation);
+
+         // Request the CurrentWeather for this station
+         getWeather(stationId);
+      })
+      .catch(error => console.log('There was a getStationId error: ', error))
+}
+
+// Gets current weather information for a specific weather station from the NWS API
+function getWeather(stationId) {
+   // This is the URL for current observation data
+   const URL = 'https://api.weather.gov/stations/' + stationId + '/observations/latest';
+   // NWS User-Agent header (build above) will be the second parameter
+   fetch(URL, idHeader)
+      .then(function (response) {
+         if (response.ok) {
+            return response.json();
+         }
+         throw new ERROR('Response not OK.');
+      })
+      .then(function (data) {
+         // Let's see what we got back
+         console.log('From getWeather function:');
+         console.log(data);
+
+         // Store weather information to localStorage
+         storage.setItem("locTemp", data.properties.temperature.value);
+         storage.setItem("locHigh", data.properties.maxTemperatureLast24Hours.value);
+         storage.setItem("locLow", data.properties.minTemperatureLast24Hours.value);
+         storage.setItem("locWind", data.properties.windSpeed.value);
+         storage.setItem("windDirection", data.properties.windDirection.value);
+         storage.setItem("WindGusts", data.properties.windGust.value);
+
+         // Build the page for viewing
+
+      })
+      .catch(error => console.log('There was a getWeather error: ', error))
+} // end getWeather function
